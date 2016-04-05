b47c069232335970a49047811aa917b607d3036a
Mark van de Korput <dr.theman@gmail.com>
2016-04-05 18:05:05 +0200

    Interfaces.add_master + test

--- py2030/interface.py
+++ py2030/interface.py
@@ -1,3 +1,4 @@
+from py2030.utils.color_terminal import ColorTerminal
 import py2030.collections.collections as Collections
 from py2030.utils.event import Event
 
@@ -16,7 +17,17 @@ class Interface:
 
     def __init__(self, options = {}):
         # attributes
+        self.masters = []
+
+        # special collections
+        # for all local changes to resource collections a new models is created in changes
         self.changes = Collections.Changes()
+        # all models to the updates collection will automatically be processed and cause
+        # the appropriate changes to the local resource collections
+        self.updates = Collections.Updates()
+
+        # resource collections are all collection for which all changes (create/update/delete)
+        # are propagated into the self.changes collection
         self.addResourceCollection(Collections.Broadcasts, 'broadcasts')
 
         # events
@@ -26,10 +37,23 @@ class Interface:
         self.options = {}
         self.configure(options)
 
+        # callbacks
+        self.updates.newModelEvent += self._onNewUpdateModel
+
     def configure(self, options):
         previous_options = self.options
         self.options.update(options)
         # TODO; any internal updates needed for the (re-)configuration happen here
+
+    def add_master(self, interface):
+        # start monitoring for any changes on the specified interface
+        interface.changes.newModelEvent += self._onNewMasterChangeModel
+        # keep list of interfaces we're syncing from
+        self.masters.append(interface)
+
+    def _onNewMasterChangeModel(self, model, col):
+        # a change in one of our sync source causes and update
+        self.updates.create(model.data)
 
     def addResourceCollection(self, cls, type):
         # create new instance of the collection
@@ -47,3 +71,13 @@ class Interface:
         # Interface 'records' all local changes (create/update/delete) to its collections
         # into the changes collection
         self.changes.create({'method': 'create', 'type': collection.serialize_name, 'data': model.data})
+
+    def _onNewUpdateModel(self, model, col):
+        try:
+            col = getattr(self, model.get('type'))
+        except AttributeError as err:
+            ColorTerminal().fail('[Interface] got update model with unknown type: ', model.get('type'))
+            return
+
+        if model.get('method') == 'create':
+            col.create(model.get('data'))
--- test/test_interface.py
+++ test/test_interface.py
@@ -46,11 +46,10 @@ class TestInterface(unittest.TestCase):
     # to any of the other collections cause a changes model to be created
     # the change model's data dict can be serialized and deserialized into/from a json format
     def test_changes(self):
-        # remember number of changes in the initial state
+        # setup
         self.interface.changes.clear()
-        # create an empty changes model
+        # create changes
         self.interface.changes.create()
-        # should appear in the list
         self.assertEqual(len(self.interface.changes), 1)
         # create another change inside the interface (add a broadcast model)
         self.interface.broadcasts.create({'foo': 'bar'})
@@ -58,7 +57,33 @@ class TestInterface(unittest.TestCase):
         self.assertEqual(len(self.interface.changes), 2)
         self.assertEqual(self.interface.changes[1].data, {'method': 'create', 'type': 'broadcasts', 'data': {'foo': 'bar'}})
 
+    def test_updates(self):
+        # setup
+        self.interface.broadcasts.clear()
+        # before
+        self.assertEqual(len(self.interface.broadcasts), 0)
+        # add 'create broadcast' update
+        self.interface.updates.create({'method': 'create', 'type': 'broadcasts', 'data': {'foo': 'bar', 'footender': 'bartender'}})
+        # result
+        self.assertEqual(len(self.interface.broadcasts), 1)
+        self.assertEqual(self.interface.broadcasts[0].data, {'foo': 'bar', 'footender': 'bartender'})
 
+    def test_add_master(self):
+        # setup
+        interface1 = Interface()
+        interface2 = Interface()
+        # before
+        self.assertEqual(len(interface2.broadcasts), 0)
+        # action
+        interface1.broadcasts.create({'id': 111})
+        interface1.broadcasts.create({'id': 222})
+        interface2.add_master(interface1)
+        interface1.broadcasts.create({'id': 333})
+        interface1.broadcasts.create({'id': 444})
+        # after
+        self.assertEqual(len(interface2.broadcasts), 2)
+        self.assertEqual(interface2.broadcasts[0].get('id'), 333)
+        self.assertEqual(interface2.broadcasts[1].get('id'), 444)
 
 
 if __name__ == '__main__':
