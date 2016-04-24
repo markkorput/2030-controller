c6829f400708dd2b54bd147dc199608453232514
Mark van de Korput <dr.theman@gmail.com>
2016-04-24 20:00:50 +0200

    client downloads config file content when getting reconfig OSC event

--- py2030/inputs/osc.py
+++ py2030/inputs/osc.py
@@ -149,6 +149,7 @@ class Osc:
         self.unknownMessageEvent(addr, tags, data, client_address, self)
 
     def _onEvent(self, addr, tags, data, client_address):
+        # print '_onEvent:', addr, tags, data, client_address
         params = json.loads(data[0])
         self.interface.genericEvent(params)
 
