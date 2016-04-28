950f4e3822b36add189cda9cbea54faa8dc0f379
Mark van de Korput <dr.theman@gmail.com>
2016-04-28 21:56:33 +0200

    --route-ip option for launcher

--- launcher.py
+++ launcher.py
@@ -24,15 +24,23 @@ class Launcher:
     def update(self):
         self.app.update()
 
-if __name__ == '__main__':
-    from optparse import OptionParser
-    parser = OptionParser()
-    parser.add_option('-p', '--profile', dest='profile', default="client")
-    parser.add_option('-c', '--client', dest='client', action="store_true", default=False)
-    parser.add_option('--install', dest='install', action="store_true", default=False)
-    parser.add_option('--bootstrap', dest='bootstrap', action="store_true", default=False)
-    options, remainder = parser.parse_args()
-    del OptionParser
+def main(options, args):
+    if options.route_ip:
+        from py2030.config_file import ConfigFile
+        cfile = ConfigFile.instance()
+        cfile.load()
+        ip = args[0] if len(args) > 0 else cfile.get_value('py2030.profiles.controller.osc_out_ip')
+        interface = args[1] if len(args) > 1 else 'en0'
+
+        if ip:
+            import os
+            os.system('sudo route -nv add -net {0} -interface {1}'.format(ip, interface))
+            ColorTerminal().success("Routed IP address {0} to interface {1}".format(ip, interface))
+            ColorTerminal().success("run `netstat -r` to check")
+        else:
+            ColorTerminal().fail("IP address could not be found in the config file, can't route")
+
+        return
 
     if options.install:
         ColorTerminal().red('--install not implemented yet')
@@ -53,3 +61,16 @@ if __name__ == '__main__':
         ColorTerminal().yellow('KeyboardInterrupt. Quitting.')
 
     launcher.destroy()
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option('-p', '--profile', dest='profile', default="client")
+    parser.add_option('-c', '--client', dest='client', action="store_true", default=False)
+    parser.add_option('--install', dest='install', action="store_true", default=False)
+    parser.add_option('--bootstrap', dest='bootstrap', action="store_true", default=False)
+    parser.add_option('--route-ip', dest="route_ip", action="store_true", help="Route IP address (default: the controller profile's osc_out_ip value from the config file) to specific interface (default: en0)", default=None)
+    options, args = parser.parse_args()
+    del OptionParser
+
+    main(options, args)
